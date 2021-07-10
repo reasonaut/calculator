dff854ecc904a8fb8974c5954285fbae24e0e7fc
Jon Garrity <jonnygarrity@gmail.com>
2021-07-09 20:58:20 -0500

    Fixed bug where previous operation was failing to evaluate in advance of updating operator to most recent keypress value

--- calculator.js
+++ calculator.js
@@ -1,3 +1,75 @@
+// holds values and operators in memory
+// todo: change numbers from an array to holding seperately two numbers
+let operand1 = NaN;
+let operand2 = NaN;
+let operator = '';
+let previousKeyWasOperator = false;
+let calcDisplayInputRecord = document.querySelector('#inputRecord');
+let calcDisplayResult = document.querySelector('#result');
+
+generateKeypad();
+
+function keyPress(eventData){
+    let pressedKey = eventData.target.innerText;
+    if (parseInt(pressedKey) <= 9){
+        if (previousKeyWasOperator) calcDisplayResult.innerText = '';
+        calcDisplayResult.innerText += pressedKey;
+        previousKeyWasOperator = false;
+        return;     
+    } else if (pressedKey === 'C'){
+        calcDisplayInputRecord.innerText = '';
+        calcDisplayResult.innerText = '';
+        operand1 = NaN;
+        operand2 = NaN;
+        numbers = [];
+    } else if (pressedKey === '='){
+        if (previousKeyWasOperator) return;
+        operand2 = parseInt(calcDisplayResult.innerText);
+        
+        previousKeyWasOperator = true;
+        equate();        
+    } else { // an operator is pressed
+        if (previousKeyWasOperator) return;
+        if (calcDisplayResult.innerText != '') {
+            previousKeyWasOperator = true;
+            // evaluate current calculation before changing operator to keyPress value
+            isNaN(operand1) ? operand1 = parseInt(calcDisplayResult.innerText) : 
+                operand2 = parseInt(calcDisplayResult.innerText);
+            calcDisplayInputRecord.innerText += `${calcDisplayResult.innerText} `;
+            if (!isNaN(operand1) && !isNaN(operand2) && operator != '') operate();
+            operator = pressedKey;
+            calcDisplayInputRecord.innerText += `${operator} `;
+            if (isNaN(operand2)) return;
+            operate();
+        } 
+    }
+}
+function equate(){
+    calcDisplayInputRecord.innerText += calcDisplayResult.innerText + ' = ';
+    operate();
+    calcDisplayInputRecord.innerText += calcDisplayResult.innerText + '; ';
+    operator = '';
+    operand1 = NaN;
+    operand2 = NaN;
+}
+// todo: operate should return a value
+function operate(){
+    if (operator === '+'){
+        const result = add(operand1, operand2);
+        console.log(operand1);
+        console.log(operand2);
+        calcDisplayResult.innerText = result;
+        operand1 = result;
+        operand2 = NaN;
+    }
+    if (operator === '-'){
+        const result = subtract(operand1, operand2);
+        calcDisplayResult.innerText = result;
+        operand1 = result;
+        operand2 = NaN;
+    }
+
+}
 // calc functions
 function add(num1, num2){
     return num1 + num2;
@@ -11,90 +83,22 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1 / num2;
 }
-// holds values and operators in memory
-let numbers = [];
-let operator = '';
-let previousKeyWasOperator = false;
-let calcDisplayInputRecord = document.querySelector('#inputRecord');
-let calcDisplayResult = document.querySelector('#result');
-
 // add buttons to each keypad div
-const keypadDivs = document.querySelectorAll('.keypadRow');
-keypadDivs.forEach(key => {
-    const btn = document.createElement('button');
-    btn.classList.add('keypadButton');
-    // label keys
-    btn.innerText = key.getAttribute('id').replace('key', '');
-    if (btn.innerText.includes('Clear')) btn.innerText = 'C';
-    if (btn.innerText.includes('Equals')) btn.innerText = '=';
-    if (btn.innerText.includes('Plus')) btn.innerText = '+';
-    if (btn.innerText.includes('Minus')) btn.innerText = '-';
-    if (btn.innerText.includes('Multiply')) btn.innerText = '*';
-    if (btn.innerText.includes('Divide')) btn.innerText = '/';
-    // add click event handlers
-    btn.addEventListener('click', keyPress);
-
-
-    
-    key.appendChild(btn);    
-});
-function keyPress(eventData){
-    let pressedKey = eventData.target.innerText;
-    if (parseInt(pressedKey) <= 9){
-        if (previousKeyWasOperator) calcDisplayResult.innerText = '';
-        calcDisplayResult.innerText += pressedKey;
-        previousKeyWasOperator = false;
-        return;     
-    } else if (pressedKey === 'C'){
-        calcDisplayInputRecord.innerText = '';
-        calcDisplayResult.innerText = '';
-        numbers = [];
-    } else if (pressedKey === '='){
-        numbers.push(parseInt(calcDisplayResult.innerText));
-        previousKeyWasOperator = true;
-        equate();        
-    } else { // an operator is pressed
-        if (calcDisplayResult.innerText != '') {
-            previousKeyWasOperator = true;
-            calcDisplayInputRecord.innerText += `${calcDisplayResult.innerText} `;
-            numbers.push(parseInt(calcDisplayResult.innerText));
-            operator = pressedKey;
-            calcDisplayInputRecord.innerText += `${operator} `;
-            operate();
-        } 
-    }
+function generateKeypad(){
+    const keypadDivs = document.querySelectorAll('.keypadRow');
+    keypadDivs.forEach(key => {
+        const btn = document.createElement('button');
+        btn.classList.add('keypadButton');
+        // label keys
+        btn.innerText = key.getAttribute('id').replace('key', '');
+        if (btn.innerText.includes('Clear')) btn.innerText = 'C';
+        if (btn.innerText.includes('Equals')) btn.innerText = '=';
+        if (btn.innerText.includes('Plus')) btn.innerText = '+';
+        if (btn.innerText.includes('Minus')) btn.innerText = '-';
+        if (btn.innerText.includes('Multiply')) btn.innerText = '*';
+        if (btn.innerText.includes('Divide')) btn.innerText = '/';
+        // add click event handlers
+        btn.addEventListener('click', keyPress);
+        key.appendChild(btn);    
+    });
 }
-function equate(){
-    if (numbers.length === 1){        
-        return;      
-    } else if (numbers.length === 2){
-        calcDisplayInputRecord.innerText += calcDisplayResult.innerText + ' = ';
-        operate();
-    }
-}
-function operate(){
-    if (numbers.length === 1){        
-        return;      
-    } else if (numbers.length === 2){
-        if (operator === '='){
-            // check operator and proceed
-            console.log(operator);
-            calcDisplayInputRecord.innerText += '= ';
-            numbers = [];
-            operator = '';
-            numbers.push(parseInt(calcDisplayResult.innerText))
-        }
-        if (operator === '+'){
-            
-            calcDisplayResult.innerText = add(numbers[0], numbers[1]);
-            console.log(numbers);
-            numbers = [];
-            operator = '';
-            numbers.push(parseInt(calcDisplayResult.innerText))
-            
-        }
-
-    }
-
-}
-
